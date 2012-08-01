6220ead82ba80cfbc3ba05b3ed12246e337bae74
m8rge <to.merge@gmail.com>
2012-08-01 17:16:21 +0600

    update sql dump

--- protected/migrations/base.sql
+++ protected/migrations/base.sql
@@ -1,11 +1,29 @@
-CREATE TABLE `AuthItem` (
-  `name` varchar(64) NOT NULL,
-  `type` int(11) NOT NULL,
-  `description` text,
-  `bizrule` text,
-  `data` text,
-  PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 3408
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.5.15)
+# Database: myarusel
+# Generation Time: 2012-08-01 11:15:45 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table AuthAssignment
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `AuthAssignment`;
 
 CREATE TABLE `AuthAssignment` (
   `itemname` varchar(64) NOT NULL,
@@ -16,6 +34,29 @@ CREATE TABLE `AuthAssignment` (
   CONSTRAINT `AuthAssignment_ibfk_1` FOREIGN KEY (`itemname`) REFERENCES `AuthItem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+
+# Dump of table AuthItem
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `AuthItem`;
+
+CREATE TABLE `AuthItem` (
+  `name` varchar(64) NOT NULL,
+  `type` int(11) NOT NULL,
+  `description` text,
+  `bizrule` text,
+  `data` text,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table AuthItemChild
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `AuthItemChild`;
+
 CREATE TABLE `AuthItemChild` (
   `parent` varchar(64) NOT NULL,
   `child` varchar(64) NOT NULL,
@@ -25,15 +66,31 @@ CREATE TABLE `AuthItemChild` (
   CONSTRAINT `AuthItemChild_ibfk_2` FOREIGN KEY (`child`) REFERENCES `AuthItem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+
+# Dump of table Carousel
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Carousel`;
+
 CREATE TABLE `Carousel` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
   `clientId` int(10) unsigned NOT NULL,
   `categories` text NOT NULL,
   `onlyCheap` int(1) NOT NULL,
+  `ownerId` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `clientId` (`clientId`)
+  KEY `clientId` (`clientId`),
+  KEY `ownerId` (`ownerId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table Client
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Client`;
 
 CREATE TABLE `Client` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -42,9 +99,18 @@ CREATE TABLE `Client` (
   `url` varchar(255) NOT NULL DEFAULT '',
   `logoUid` varchar(255) NOT NULL DEFAULT '',
   `caption` varchar(255) NOT NULL DEFAULT '',
-  `color` int(11) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `color` char(6) DEFAULT NULL,
+  `ownerId` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ownerId` (`ownerId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table Item
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Item`;
 
 CREATE TABLE `Item` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -57,6 +123,13 @@ CREATE TABLE `Item` (
   KEY `carouselId` (`carouselId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+
+# Dump of table User
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `User`;
+
 CREATE TABLE `User` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
@@ -65,3 +138,13 @@ CREATE TABLE `User` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
