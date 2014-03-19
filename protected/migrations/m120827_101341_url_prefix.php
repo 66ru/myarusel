c2b980b45b7fd5ed378cb3911ba09e7813054f57
nikll <nikll@rambler.ru>
2014-03-19 10:52:26 +0600

    Правка имен таблиц (в бд название в верхнем регистре тогда как в миграциях почему то в нижнем)

--- protected/migrations/m120827_101341_url_prefix.php
+++ protected/migrations/m120827_101341_url_prefix.php
@@ -4,12 +4,12 @@ class m120827_101341_url_prefix extends CDbMigration
 {
 	public function up()
 	{
-		$this->addColumn('carousel','urlPrefix','varchar(255) not null');
+		$this->addColumn('Carousel','urlPrefix','varchar(255) not null');
 	}
 
 	public function down()
 	{
-		$this->dropColumn('carousel','urlPrefix');
+		$this->dropColumn('Carousel','urlPrefix');
 	}
 
 }
