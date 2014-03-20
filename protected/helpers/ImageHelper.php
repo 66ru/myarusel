2da30ce7fe031866658ffe5692f3866e70481fd1
nikll <nikll@rambler.ru>
2014-03-20 14:35:08 +0600

    Костыль для source-based nix-систем, по дефолту пути поиска bin:/usr/bin тогда как весь софт из портов ставится в /usr/local/bin

--- protected/helpers/ImageHelper.php
+++ protected/helpers/ImageHelper.php
@@ -1,19 +1,14 @@
 <?php
 
-class ImageHelper
-{
-	public static function checkImageCorrect($imagePath)
-	{
-		$cmd = "identify -format \"%w|%h|%k\" ".escapeshellarg($imagePath)." 2>&1";
+class ImageHelper {
+	public static function checkImageCorrect($imagePath) {
+		$cmd       = "PATH=\$PATH:/usr/local/bin identify -format \"%w|%h|%k\" ".escapeshellarg($imagePath)." 2>&1";
 		$returnVal = 0;
-		$output = array();
+		$output    = array();
 		exec($cmd, $output, $returnVal);
-		if ($returnVal == 0 && count($output) == 1) {
-			return true;
-		} elseif ($returnVal == 127) {
-			throw new CException('Can\'t find identify');
-		} else {
-			return false;
-		}
+
+		if ($returnVal == 0 && count($output) == 1) return true;
+		if ($returnVal == 127) throw new CException('Can\'t find identify');
+		return false;
 	}
 }
