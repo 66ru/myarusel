b49791269d02b95c845851d9d85a8faf9819bb2b
m8rge <to.merge@gmail.com>
2014-07-30 11:50:49 +0600

    startTaskButton fix

--- protected/extensions/asyncTask/widget/StartTaskButton.php
+++ protected/extensions/asyncTask/widget/StartTaskButton.php
@@ -37,39 +37,61 @@ class StartTaskButton extends CWidget
      */
     public $buttonProperties = [];
 
+    /**
+     * @var array
+     */
+    public $htmlOptions = [];
+
     public function run()
     {
         $encodedWaitingLabel = json_encode($this->waitingLabel);
         $encodedLabel = json_encode($this->label);
+        $encodedStatusRoute = json_encode(CHtml::normalizeUrl($this->taskStatusRoute));
+        $buttonClass = 'start-task-button';
+
+        if (isset($this->htmlOptions['class'])) {
+            $this->htmlOptions['class'] .= ' ' . $buttonClass;
+        } else {
+            $this->htmlOptions['class'] = $buttonClass;
+        }
         $this->widget('bootstrap.widgets.TbButton', CMap::mergeArray([
-                'buttonType' => 'ajaxButton',
+                'buttonType' => 'url',
                 'label' => $this->label,
+                'htmlOptions' => $this->htmlOptions,
                 'url' => CHtml::normalizeUrl($this->startTaskRoute),
-                'ajaxOptions' => [
-                    'context' => new CJavaScriptExpression('this'),
-                    'complete' => 'function() {
-                        $(this).html('.$encodedWaitingLabel.').attr("disabled", "disabled");
-                    }',
-                    'success' => 'function(data) {
-                        var $button = $(this);
+            ], $this->buttonProperties));
+
+        Yii::app()->clientScript->registerScript(__CLASS__, "
+            jQuery('body').on('click', '.$buttonClass', function() {
+                jQuery.ajax({
+                    context: this,
+                    url: $(this).attr('href'),
+                    cache: false,
+                    complete: function() {
+                        $(this).html($encodedWaitingLabel).attr('disabled', 'disabled');
+                    },
+                    success: function (data) {
+                        var \$button = $(this);
                         var taskId = data.taskId;
                         var checkStatus = function() {
-                            $.getJSON("'.CHtml::normalizeUrl($this->taskStatusRoute).'", { taskId: taskId }, function(task) {
+                            $.getJSON($encodedStatusRoute, { taskId: taskId }, function(task) {
                                 if (task.status == 1) {
-                                    $button.text('.$encodedLabel.').removeAttr("disabled");
-                                    ('.$this->doneAction.')($button, task.result);
+                                    \$button.text($encodedLabel).removeAttr('disabled');
+                                    ($this->doneAction)(\$button, task.result);
                                 } else if (task.status == 0) {
-                                    setTimeout(checkStatus, '.$this->checkStatusTimeout.');
+                                    setTimeout(checkStatus, $this->checkStatusTimeout);
                                 }
                             })
                             .fail(function() {
-                                setTimeout(checkStatus, '.$this->checkStatusTimeout.');
+                                setTimeout(checkStatus, $this->checkStatusTimeout);
                             });
                         };
 
-                        setTimeout(checkStatus, '.$this->checkStatusTimeout.');
-                    }',
-                ],
-            ], $this->buttonProperties));
+                        setTimeout(checkStatus, $this->checkStatusTimeout);
+                    },
+                })
+                return false;
+            });
+        ");
     }
 }
